c650411a36942343c9dcd68187ec3438ffb840c0
Ekaterina Golubeva <117741432+Peaceandmaths@users.noreply.github.com>
2024-08-03 00:18:47 +0200

    Delete script_057_2_internal.sh

--- script_057_2_internal.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Evaluation internal test set fold 2"
-nnUNetv2_evaluate_folder -djfile /data/golubeka/nnUNet_Frame/nnUNet_data/nnUNet_raw/Dataset057_IA/dataset.json -pfile /data/golubeka/nnUNet_Frame/nnUNet_data/nnUNet_results/Dataset057_IA/nnUNetTrainer__nnUNetPlans__3d_fullres/plans.json --chill /data/golubeka/nnUNet_Frame/nnUNet_data/nnUNet_raw/Dataset057_IA/labelsTs_internal /data/golubeka/nnUNet_Frame/nnUNet_data/nnUNet_results/Dataset057_IA/postprocessed_internal_f2
-echo "Evaluation internal completed successfully"
